dd9a5587c9473e87bc6b963c198f7565c19dbfe2
Edwin <Edwin.X.Rivera.-nd@disney.com>
2021-05-08 16:29:12 +0800

    vanilla angular setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule
